c954515df38a8e67400c782bbad13c0f6340533b
hutoroff <hutoroff@gmail.com>
2016-07-18 22:49:38 +0300

    Changed type of id  for MoneyTransactionType entity: short -> Integer equals, hashCode functions are regenerated

--- hermes-jpa/src/main/java/ru/hutoroff/hermes/model/MoneyTransactionType.java
+++ hermes-jpa/src/main/java/ru/hutoroff/hermes/model/MoneyTransactionType.java
@@ -8,17 +8,17 @@ import javax.persistence.*;
 @Entity
 @Table(name = "TRANSACTION_TYPES", schema = "hermes", catalog = "test_db")
 public class MoneyTransactionType {
-    private short id;
+    private Integer id;
     private String name;
     private Boolean destNeeded;
 
     @Id
     @Column(name = "ID", nullable = false)
-    public short getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(short id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -45,22 +45,21 @@ public class MoneyTransactionType {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof MoneyTransactionType)) return false;
 
         MoneyTransactionType that = (MoneyTransactionType) o;
 
-        if (id != that.id) return false;
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
-        if (destNeeded != null ? !destNeeded.equals(that.destNeeded) : that.destNeeded != null) return false;
+        if (!getId().equals(that.getId())) return false;
+        if (!getName().equals(that.getName())) return false;
+        return getDestNeeded().equals(that.getDestNeeded());
 
-        return true;
     }
 
     @Override
     public int hashCode() {
-        int result = (int) id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (destNeeded != null ? destNeeded.hashCode() : 0);
+        int result = getId().hashCode();
+        result = 31 * result + getName().hashCode();
+        result = 31 * result + getDestNeeded().hashCode();
         return result;
     }
 }
